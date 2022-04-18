fa611083bff9ac31070c59daee0ccc2409b4ffa7
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2022-04-18 13:12:05 +0200

    moved Type struct to different mod

--- src/shell/value.rs
+++ src/shell/value.rs
@@ -1,6 +1,5 @@
-use std::{fmt, hash::Hash, ops::Range};
+use std::{fmt, ops::Range};
 
-use bitflags::bitflags;
 use indexmap::IndexMap;
 use yansi::Paint;
 
@@ -10,98 +9,8 @@ use crate::parser::{ast::expr::binop::BinOp, shell_error::ShellErrorKind, P};
 mod format;
 pub mod table;
 use table::Table;
-
-bitflags! {
-    #[rustfmt::skip]
-    pub struct Type: u16 {
-        const NULL =        0b0000000001;
-        const INT =         0b0000000010;
-        const FLOAT =       0b0000000100;
-        const BOOL =        0b0000001000;
-        const STRING =      0b0000010000;
-        const LIST =        0b0000100000;
-        const MAP =         0b0001000000;
-        const TABLE =       0b0010000000;
-        const RANGE =       0b0100000000;
-        const VALUESTREAM = 0b1000000000;
-    }
-}
-
-impl fmt::Display for Type {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut is_first = false;
-
-        if self.intersects(Self::NULL) {
-            write!(f, "'null'")?;
-            is_first = true;
-        }
-
-        if self.intersects(Self::INT) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'int'")?;
-        }
-
-        if self.intersects(Self::FLOAT) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'float'")?;
-        }
-
-        if self.intersects(Self::STRING) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'string'")?;
-        }
-
-        if self.intersects(Self::LIST) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'list'")?;
-        }
-
-        if self.intersects(Self::MAP) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'map'")?;
-        }
-
-        if self.intersects(Self::TABLE) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'table'")?;
-        }
-
-        if self.intersects(Self::RANGE) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            is_first = true;
-            write!(f, "'range'")?;
-        }
-
-        if self.intersects(Self::VALUESTREAM) {
-            if is_first {
-                write!(f, " or ")?;
-            }
-            write!(f, "'stream'")?;
-        }
-
-        Ok(())
-    }
-}
+mod types;
+pub use types::Type;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
