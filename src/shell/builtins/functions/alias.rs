055a9c7b5c6ac85bfeca82d06434d17025e39fa6
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2023-09-11 20:12:33 +0200

    make alias command better

--- src/shell/builtins/functions/alias.rs
+++ src/shell/builtins/functions/alias.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 
+use indexmap::IndexMap;
 use once_cell::sync::Lazy;
 
 use crate::{
@@ -8,7 +9,7 @@ use crate::{
     shell::{
         frame::Frame,
         stream::{OutputStream, ValueStream},
-        value::{SpannedValue, Type},
+        value::{table::Table, SpannedValue, Type, Value},
         Shell,
     },
 };
@@ -16,16 +17,8 @@ use crate::{
 static APP: Lazy<App> = Lazy::new(|| {
     App::new("alias")
         .about("Set alias")
-        .arg(
-            Arg::new("NAME", Type::STRING)
-                .help("Name of the alias")
-                .required(true),
-        )
-        .arg(
-            Arg::new("COMMAND", Type::STRING)
-                .help("The command that will be run")
-                .required(true),
-        )
+        .arg(Arg::new("NAME", Type::STRING).help("Name of the alias"))
+        .arg(Arg::new("COMMAND", Type::STRING).help("The command that will be run"))
 });
 
 pub fn alias(
@@ -44,39 +37,62 @@ pub fn alias(
         Err(e) => return Err(e.into()),
     };
 
-    let name = matches.take_value("NAME").unwrap().unwrap_string();
-    let command = matches.take_value("COMMAND").unwrap().unwrap_string();
+    let name = matches.take_value("NAME").map(|s| s.unwrap_string());
+    let command = matches.take_value("COMMAND").map(|s| s.unwrap_string());
 
-    if name.is_empty() {
-        return Err(ShellErrorKind::Basic(
-            "Alias Error",
-            format!(
-                "alias [NAME] must be atleast one character long\n\n{}",
-                APP.usage()
-            ),
-        ));
+    if let Some(name) = name {
+        if name.is_empty() {
+            return Err(ShellErrorKind::Basic(
+                "Alias Error",
+                format!(
+                    "alias [NAME] must be atleast one character long\n\n{}",
+                    APP.usage()
+                ),
+            ));
+        }
+
+        if let Some(command) = command {
+            if command.is_empty() {
+                return Err(ShellErrorKind::Basic(
+                    "Alias Error",
+                    format!(
+                        "alias [COMMAND] must be atleast one character long\n\n{}",
+                        APP.usage()
+                    ),
+                ));
+            }
+
+            // TODO this should reject anything with a word break char in
+            if name.chars().any(|c| c.is_whitespace()) {
+                return Err(ShellErrorKind::Basic(
+                    "Alias Error",
+                    format!("alias [NAME] cannot contain whitespace\n\n{}", APP.usage()),
+                ));
+            }
+
+            shell
+                .aliases
+                .insert(Rc::unwrap_or_clone(name), Rc::unwrap_or_clone(command));
+        } else if let Some(command) = shell.aliases.get(&*name) {
+            output.push(Value::from(command.clone()));
+        } else {
+            return Err(ShellErrorKind::Basic(
+                "Alias Error",
+                format!("Unknown alias: `{name}`\n\n{}", APP.usage()),
+            ));
+        }
+    } else {
+        let mut table = Table::new();
+        let alias_header: Rc<str> = Rc::from("alias");
+        let command_header: Rc<str> = Rc::from("command");
+        for (alias, command) in &shell.aliases {
+            table.insert_map(IndexMap::from([
+                (alias_header.clone(), Value::from(alias.clone())),
+                (command_header.clone(), Value::from(command.clone())),
+            ]));
+        }
+        output.push(table.into());
     }
 
-    if command.is_empty() {
-        return Err(ShellErrorKind::Basic(
-            "Alias Error",
-            format!(
-                "alias [COMMAND] must be atleast one character long\n\n{}",
-                APP.usage()
-            ),
-        ));
-    }
-
-    // TODO this should reject anything with a word break char in
-    if name.chars().any(|c| c.is_whitespace()) {
-        return Err(ShellErrorKind::Basic(
-            "Alias Error",
-            format!("alias [NAME] cannot contain whitespace\n\n{}", APP.usage()),
-        ));
-    }
-
-    shell
-        .aliases
-        .insert(Rc::unwrap_or_clone(name), Rc::unwrap_or_clone(command));
     Ok(())
 }
