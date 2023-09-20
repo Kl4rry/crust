fee80ed1b63af85d414cf5fcb91affdcb52c762d
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2023-09-20 21:07:11 +0200

    simplify dir history

--- src/shell/dir_history.rs
+++ src/shell/dir_history.rs
@@ -4,42 +4,27 @@ use crate::parser::shell_error::ShellErrorKind;
 
 pub struct DirHistory {
     dir_history: Vec<PathBuf>,
-    dir_history_index: i64,
 }
 
 impl DirHistory {
     pub fn new() -> Self {
         Self {
             dir_history: Vec::new(),
-            dir_history_index: -1,
         }
     }
 
     pub fn change_dir(&mut self, dir: impl AsRef<Path>) -> Result<(), ShellErrorKind> {
         let old_dir = std::env::current_dir().map_err(|err| ShellErrorKind::Io(None, err))?;
-        self.push_dir(old_dir);
+        self.dir_history.push(old_dir);
 
         std::env::set_current_dir(dir).map_err(|err| ShellErrorKind::Io(None, err))
     }
 
-    fn push_dir(&mut self, dir: PathBuf) {
-        if self.dir_history_index + 1 < self.dir_history.len() as i64 {
-            for _ in 0..(self.dir_history.len() as i64 - self.dir_history_index + 1) {
-                self.dir_history.pop();
-            }
-        }
-        self.dir_history.push(dir);
-        self.dir_history_index += 1;
-    }
-
     pub fn back(&mut self) -> Result<(), ShellErrorKind> {
-        if self.dir_history_index < 0 {
-            return Ok(());
+        if let Some(path) = self.dir_history.pop() {
+            std::env::set_current_dir(path).map_err(|err| ShellErrorKind::Io(None, err))?;
         }
-
-        let path = &self.dir_history[self.dir_history_index as usize];
-        self.dir_history_index -= 1;
-        std::env::set_current_dir(path).map_err(|err| ShellErrorKind::Io(None, err))
+        Ok(())
     }
 }
 
